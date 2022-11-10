0c6e0453e983079163ebb8cae9438f236ac65dbf
breezo <breezojr@gmail.com>
2022-11-10 12:23:48 +0300

    ✨ Home page implementation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,54 @@
 import MainLayout from './layout/Layout'
 import styles from './styles/home.module.css'
+import logo from '../logo.svg';
+
 const Home = () => {
 
     return (
         <MainLayout>
             <div className={styles.home}>
-                <div className={styles.navibar}>
+                <div className={styles.navbar}>
                     <div className={styles.title}>
                         <p>Book Library</p>
                     </div>
-                    <div className={styles.container}>
+                    <div className={styles.navs}>
                         <p>Home</p>
                         <p>Login</p>
                         <p>Logout </p>
                     </div>
                     <div className={styles.profile}>
+                        <div className={styles.pro_container}>
+                            <img src={logo} alt="" />
+                        </div>
 
+                    </div>
+                </div>
+                <div className={styles.main}>
+                    <div className={styles.title}>
+                        <p>Books</p>
+                    </div>
+                    <div className={styles.container}>
+                        <div className={styles.row}>
+                            <p className={styles.rtitle}>Apocalypto</p>
+                            <p className={styles.author}>some Auther</p>
+                            <p className={styles.description}>kanamakbdbc;dbfelhfblad fbefvandgfvb,fvf;fbdfjdfvlfgdfbhdfgv</p>
+                        </div>
+                        <div className={styles.row}>
+                            <p className={styles.rtitle}>Apocalypto</p>
+                            <p className={styles.author}>some Auther</p>
+                            <p className={styles.description}>kanamakbdbc;dbfelhfblad fbefvandgfvb,fvf;fbdfjdfvlfgdfbhdfgv</p>
+                        </div>
+                        <div className={styles.row}>
+                            <p className={styles.rtitle}>Apocalypto</p>
+                            <p className={styles.author}>some Auther</p>
+                            <p className={styles.description}>kanamakbdbc;dbfelhfblad fbefvandgfvb,fvf;fbdfjdfvlfgdfbhdfgv</p>
+                        </div>
+                        <div className={styles.row}>
+                            <p className={styles.rtitle}>Apocalypto</p>
+                            <p className={styles.author}>some Auther</p>
+                            <p className={styles.description}>kanamakbdbc;dbfelhfblad fbefvandgfvb,fvf;fbdfjdfvlfgdfbhdfgv</p>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
